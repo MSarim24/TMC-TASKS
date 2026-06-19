3e0c901998eb87ca9285391b1d897cd5bb1f511e
MSarim24 <msarimhusain@gmail.com>
2026-06-19 16:25:35 +0500

    TASK 8

--- TASK 8/bank.py
+++ TASK 8/bank.py
@@ -3,46 +3,48 @@ import json
 class BankAccount:
     def __init__(self, owner=""):
         self.owner = owner
-        self.balance = 0.0
-        self.transactions = []
-    
-    def deposit(self, amount):
-        self.balance += amount
-        self.transactions.append(f"Deposited: {amount}")
-        self.save_to_file("bank_account.json") 
-    
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient funds.")
-            return
-        self.balance -= amount
-        self.transactions.append(f"Withdrew: {amount}")
-        self.save_to_file("bank_account.json") 
+        self.__balance = 0.0
+        self.__transactions = []
 
+    def deposit(self, amount):
+        self.__balance += amount
+        self.__transactions.append(f"Deposit: {amount}")
+        self.save_to_file("bank_account.json")
+
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            print("Insufficient Balance ")
+            return
+        self.__balance -= amount
+        self.__transactions.append(f"Withdrew: {amount}")
+        self.save_to_file("bank_account.json")
     
     def display_balance(self):
-        print(f"Owner:{self.owner}\nCurrent Balance: {self.balance}")
-    
+        print("\n\n---DISPLAYING BALANCE---")
+        print(f"{self.owner}: {self.__balance}")
+
     def display_transactions(self):
-        print("Transaction History:")
-        for transaction in self.transactions:
+        print("\n\nTransaction History:")
+        for transaction in self.__transactions:
             print(transaction)
-    
-    def save_to_file(self, filename):
+
+    def save_to_file(self,filename):
         data = {
-            "owner": self.owner,
-            "balance": self.balance,
-            "transactions": self.transactions
+            "owner" : self.owner,
+            "balance": self.__balance,
+            "transactions" : self.__transactions
         }
-        with open(filename, 'w') as file:
-            json.dump(data, file)
-    
-    def load_from_file(self, filename):
-        with open(filename, 'r') as file:
+
+        with open(filename,'w') as file:
+            json.dump(data,file)
+
+    def load_from_file(self,filename):
+        with open(filename,'r') as file:
             data = json.load(file)
-            self.owner = data["owner"]
-            self.balance = data["balance"]
-            self.transactions = data["transactions"]
+            self.owner =  data["owner"]
+            self.__balance = data["balance"]
+            self.__transactions = data["transactions"]
+
 
 account = BankAccount("John Doe")
 account.deposit(100)
